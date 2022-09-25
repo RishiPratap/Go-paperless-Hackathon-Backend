8450346792164a01ce1a5fa06802cd3f77dbb24c
parthusun8 <parthusun8@gmail.com>
2022-09-25 20:29:11 +0530

    All bugs fixed regarding creating applications

--- controllers/dropboxController.js
+++ controllers/dropboxController.js
@@ -1,7 +1,16 @@
 var fetch = require("isomorphic-fetch");
 var Dropbox = require("dropbox").Dropbox;
 const { db } = require("../firebase");
-const { getDocs, getDoc, collection, doc, setDoc } = require("firebase/firestore");
+const {
+  getDocs,
+  getDoc,
+  collection,
+  doc,
+  setDoc,
+} = require("firebase/firestore");
+var hellosign = require("hellosign-sdk")({
+  key: "c9fec5e3c70d8fa2fb91a26c1426c707916de20fed40b2d845aab968b0edc93b",
+});
 
 // dropbox/viewapplication  -- POST
 const viewApplications = async (req, res) => {
@@ -11,7 +20,12 @@ const viewApplications = async (req, res) => {
       db,
       `users/${req.body?.email.split("@")[0]}/Applications`
     );
-    const colRef2 = collection(db, 'users', req.body?.email.split("@")[0], 'Inbox');
+    const colRef2 = collection(
+      db,
+      "users",
+      req.body?.email.split("@")[0],
+      "Inbox"
+    );
     const docSnap = await getDocs(colRef);
     const inboxSnap = await getDocs(colRef2);
     const docList = docSnap.docs.map((doc) => doc.data());
@@ -21,10 +35,14 @@ const viewApplications = async (req, res) => {
 
     const applications = [];
     docList.forEach((doc) => {
-      applications.push({ name: doc.alias, status: doc.status});
+      applications.push({ name: doc.alias, status: doc.status });
     });
     inboxList.forEach((doc) => {
-      applications.push({ name: doc.application_name, requester: doc.requester, status : "Inbox"});
+      applications.push({
+        name: doc.application_name,
+        requester: doc.requester,
+        status: "Inbox",
+      });
     });
     console.log(applications);
     res.status(200).send(applications);
@@ -37,9 +55,9 @@ const viewApplications = async (req, res) => {
 const getFileList = async (req, res) => {
   //{acessToken, path}
   console.log(req.body);
-  const refPath = `users/${
-    req.body.email.split("@")[0]
-  }/Applications/${req.body.path}`;
+  const refPath = `users/${req.body.email.split("@")[0]}/Applications/${
+    req.body.path
+  }`;
   const docRef = doc(db, refPath);
   // console.log(docRef);
   const userSnapshot = await getDoc(docRef);
@@ -48,7 +66,7 @@ const getFileList = async (req, res) => {
   let response = {
     current_hop: data_user.current_hop,
     total_hops: data_user.total_signers,
-    signers : data_user.signers,
+    signers: data_user.signers,
     files: [],
   };
   var dbx = new Dropbox({ accessToken: req.body?.accessToken, fetch: fetch });
@@ -60,7 +78,9 @@ const getFileList = async (req, res) => {
       console.log(resp?.result?.entries);
       const data = await resp?.result?.entries;
       // response.files = data;
-      data.forEach((file) => {response.files.push(file.name)});
+      data.forEach((file) => {
+        response.files.push(file.name);
+      });
       res.status(200).send(response);
     })
     .catch((err) => {
@@ -69,12 +89,13 @@ const getFileList = async (req, res) => {
       console.log(errorMsg);
       res.status(500).send(errorMsg);
     });
-  
 };
 
 //dropbox/createapplication  -- POST
 const createApplication = async (req, res) => {
-  // {accessToken, appName, username, applType, signers(array)}
+  // {accessToken, appName, signers(array), email}
+  req.body.signers = [req.body.email, ...req.body.signers];
+  console.log(req.body);
   var dbx = new Dropbox({ accessToken: req.body?.accessToken, fetch: fetch });
   dbx
     .fileRequestsCreate({
@@ -83,28 +104,55 @@ const createApplication = async (req, res) => {
     })
     .then(async (resp) => {
       console.log(resp);
+      res.status(200).send(resp.result.url);
+
+      // HELLOSIGN
+      var options = {
+        test_mode: 1,
+        clientId: "7c6cd4722bf993f3508d03033f104bce",
+        subject: "Subject Here",
+        title: "Title Here",
+        message: "Message Here",
+        signers: [],
+        file_url: [
+          "https://www.dropbox.com/s/1q3aw1wltxpswbl/Parth%20Sundarka%20-%20WhatsApp%20Image%202022-09-15%20at%206.58.06%20PM.jpeg?dl=0",
+        ],
+      };
+      for (let i = 0; i < req.body.signers.length; i++) {
+        options.signers.push({
+          email_address: req.body.signers[i],
+          order: i,
+          name: req.body.signers[i].split("@")[0],
+        });
+      }
+      let helloData = {};
+      await hellosign.signatureRequest.createEmbedded(options).then(async (resp) => {
+        console.log(resp);
+        helloData = resp.signature_request;
+        console.log("Done Successfully");
+      });
 
       // ADD FIREBASE CODE HERE
       const docRef = doc(
         db,
-        `users/${req.body?.username}/Applications/${req.body?.appName}`
+        `users/${req.body?.email.split("@")[0]}/Applications/${
+          req.body?.appName
+        }`
       );
       const application_details = {
         url: resp.result?.url,
         total_signers: req.body?.signers.length,
         signers: req.body?.signers,
         status: "Pending",
-        type: req.body?.applType,
         alias: req.body?.appName,
         current_hop: 0,
+        signature_request: helloData,
       };
       console.log(application_details);
       await setDoc(docRef, application_details);
-
-      res.status(200).send(resp.result.url);
     })
     .catch((err) => {
-      console.log(err?.message);
+      console.log("Error", err?.message);
       res.status(500).send(err?.message);
     });
 };
--- controllers/helloSignController.js
+++ controllers/helloSignController.js
@@ -22,53 +22,54 @@ const verify_account = async (req, res) => {
     });
 };
 
-const create_application = async (req, res) => {
-  // {email, application_name, signers(array)}
-  var options = {
-    test_mode: 1,
-    clientId: "7c6cd4722bf993f3508d03033f104bce",
-    subject: "Subject Here",
-    title: "Title Here",
-    message: "Message Here",
-    signers: [
-      {
-        email_address: "parthusun8@gmail.com",
-        name: "Parth",
-        order: 0,
-      },
-      {
-        email_address: "rp0256@gmail.com",
-        name: "Rishi",
-        order: 1,
-      },
-      {
-        email_address: "ps2644@srmist.edu.in",
-        name: "Srihari",
-        order: 2,
-      },
-    ],
-    file_url: [
-      "https://www.dropbox.com/s/1q3aw1wltxpswbl/Parth%20Sundarka%20-%20WhatsApp%20Image%202022-09-15%20at%206.58.06%20PM.jpeg?dl=0",
-    ],
-  };
-  let responseData = {};
-  hellosign.signatureRequest
-    .createEmbedded(options)
-    .then(async (resp) => {
-      console.log(resp);
-      responseData = resp;
-      const docRef = doc(db, `users/${req.body.email.split("@")[0]}/Applications/${req.body.application_name}`);
-      await updateDoc(docRef, {signature_request   : responseData.signature_request});
-    console.log("Done Successfully");
-      res.status(200).send(resp.signature_request);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).send(err);
-    });
-};
+// const create_application = async (req, res) => {
+//   // {email, application_name, signers(array)}
+//   var options = {
+//     test_mode: 1,
+//     clientId: "7c6cd4722bf993f3508d03033f104bce",
+//     subject: "Subject Here",
+//     title: "Title Here",
+//     message: "Message Here",
+//     signers: [
+//       {
+//         email_address: "parthusun8@gmail.com",
+//         name: "Parth",
+//         order: 0,
+//       },
+//       {
+//         email_address: "rp0256@gmail.com",
+//         name: "Rishi",
+//         order: 1,
+//       },
+//       {
+//         email_address: "ps2644@srmist.edu.in",
+//         name: "Srihari",
+//         order: 2,
+//       },
+//     ],
+//     file_url: [
+//       "https://www.dropbox.com/s/1q3aw1wltxpswbl/Parth%20Sundarka%20-%20WhatsApp%20Image%202022-09-15%20at%206.58.06%20PM.jpeg?dl=0",
+//     ],
+//   };
+//   let responseData = {};
+//   hellosign.signatureRequest
+//     .createEmbedded(options)
+//     .then(async (resp) => {
+//       console.log(resp);
+//       responseData = resp;
+//       const docRef = doc(db, `users/${req.body.email.split("@")[0]}/Applications/${req.body.application_name}`);
+//       await updateDoc(docRef, {signature_request   : responseData.signature_request});
+//     console.log("Done Successfully");
+//       res.status(200).send(resp.signature_request);
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//       res.status(500).send(err);
+//     });
+// };
 
 //hellosign/getsignurl
+
 const get_sign_url = async (req, res) => {
   // {email, application_name}
   try{
@@ -90,6 +91,6 @@ const get_sign_url = async (req, res) => {
 
 module.exports = {
   verify_account,
-  create_application,
+  // create_application,
   get_sign_url,
 };
